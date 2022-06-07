8f236fcd8b0aa33402fc009914818c8885d6867b
yaikob-12 <jacob121921@gmail.com>
2022-06-07 13:58:41 +0300

    jack

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+
+        char ch[10] = "_putchar ";
+	int i=0;
+
+        for(i=0 ; i<10 ; i++)
+        {
+          putchar (ch[i]);
+        }
+        putchar ('\n');
+        return 0;
+        }
+
+
